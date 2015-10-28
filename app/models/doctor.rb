8095ed3e1ff80dda0d098593ea7c68d7bd01e09d
agush <agush@roca9.com>
2015-10-28 05:01:27 -0600

    Add devise for customers, admin, doctors, add pages controller for root path

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,7 @@
 class Doctor < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
   validates :name, presence: true
   validates :name, length: { maximum: 35 }
   validates :zip, length: { maximum: 5 }
