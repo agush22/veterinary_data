1f7315feacc4b1dcccf52e67cebfccd0d04a76bb
agush <agush@roca9.com>
2015-10-28 15:00:02 -0600

    Set root routes for different users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :pets
   devise_for :customers
   devise_for :admins
   devise_for :doctors
+  resources :appointments
+  resources :pets
   resources :doctors
+
+  get '/doctors' => "doctors#index", as: :admin_root
+  get '/appointments/new' => "appointments#new", as: :receptionist_root
+  get '/pets/new' => "pets#new", as: :doctor_root
+  get '/customer_appointments'=> "appointments#customer", as: :customer_root
 
   root 'pages#home'
 end
